b821e3d11dc5e39b99962764f99e0e2a5008bc8f
TuanKenOfficial <nhatyeuthu2013@gmail.com>
2024-10-18 13:59:58 +0700

    Update

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,16 +1,20 @@
-
 import 'package:get/get.dart';
 import 'package:my_grocery/model/ad_banner.dart';
+import 'package:my_grocery/model/category.dart';
 import 'package:my_grocery/service/remote_service/remote_banner_service.dart';
+import 'package:my_grocery/service/remote_service/remote_popular_category_service.dart';
 
 class HomeController extends GetxController{
   static HomeController instance = Get.find();
   RxList<AdBanner> bannerList = List<AdBanner>.empty(growable: true).obs;
+  RxList<Category> popularCategoryList = List<Category>.empty(growable: true).obs;
   RxBool isBannerLoading = false.obs;
+  RxBool isPopularCategoryLoading = false.obs;
 
   @override
   void onInit() {
     getAdBanners();
+    getPopularCategories();
     super.onInit();
   }
 
@@ -25,6 +29,24 @@ class HomeController extends GetxController{
       // print(bannerList.length);
       // print(bannerList.first.image);
       isBannerLoading(false);
+    }
+  }
+  void getPopularCategories() async {
+    try {
+      isPopularCategoryLoading(true);
+      // if (_localCategoryService.getPopularCategories().isNotEmpty) {
+      //   popularCategoryList
+      //       .assignAll(_localCategoryService.getPopularCategories());
+      // }
+      var result = await RemotePopularCategoryService().get();
+      if (result != null) {
+        popularCategoryList.assignAll(popularCategoryListFromJson(result.body));
+        // _localCategoryService.assignAllPopularCategories(
+        //     popularCategories: popularCategoryListFromJson(result.body));
+      }
+    } finally {
+      print(popularCategoryList.length);
+      isPopularCategoryLoading(false);
     }
   }
 }
--- lib/view/home/home_screens.dart
+++ lib/view/home/home_screens.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_grocery/component/main_header.dart';
 import 'package:my_grocery/controller/controllers.dart';
+import 'package:my_grocery/view/home/components/carousel_slider/carousel_slider_view.dart';
+import 'package:my_grocery/view/home/components/popular_category/popular_category_loading.dart';
 
 import 'components/carousel_slider/carousel_loading.dart';
 
@@ -10,18 +12,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column(
-        children: [
-          MainHeader(),
-          Obx((){
-            if(homeController.isBannerLoading.value){
-              return CarouselLoading();
-            }else{
-              return CarouselLoading();
-            }
-          })
-        ],
-    )
-    );
+    return SafeArea(
+        child: Column(
+      children: [
+        MainHeader(),
+
+        //banner
+        Obx(() {
+          if (homeController.bannerList.isNotEmpty) {
+            return CarouselSliderView(bannerList: homeController.bannerList);
+          } else {
+            return CarouselLoading();
+          }
+        }),
+
+        //popular category
+        Obx(() {
+          if (homeController.popularCategoryList.isNotEmpty) {
+            return PopularCategoryLoading();
+          } else {
+            return PopularCategoryLoading();
+          }
+        })
+      ],
+    ));
   }
 }
